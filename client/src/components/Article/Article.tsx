18bbd082029e385fba3f4934174d0220c143a2fd
Julien Ido <idojulien202@gmail.com>
2024-11-08 17:05:28 +0100

    J'ai fait le css du composant Article  et j'ai changer les photos du restaurant et du plats far

--- client/src/components/Article/Article.tsx
+++ client/src/components/Article/Article.tsx
@@ -1,59 +1,74 @@
+import "./Article.css";
 function Article() {
   return (
     <>
-      <header>
-        <img
-          src="/public/images/DALL·E-2024-11-05-11.41.34-A-logo-for-a-restaurant-named-_Flandresto_-with-a-minimalistic-design.png"
-          alt="logo"
+      <img
+        className="logo"
+        src="/public/images/DALL·E-2024-11-05-11.41.34-A-logo-for-a-restaurant-named-_Flandresto_-with-a-minimalistic-design.png"
+        alt="logo"
+      />
+      <section className="searchbar-article">
+        <input
+          type="text"
+          name="searchBar"
+          id="searchBar"
+          placeholder="Recherche"
         />
-        <section>
-          <input
-            type="text"
-            name="searchBar"
-            id="searchBar"
-            placeholder="Recherche"
-          />
-        </section>
-      </header>
-      <main>
-        <section>
-          <img
-            src="/public/images/photoplatrestautest.png"
-            alt="photo-plat"
-          />
-          <img
-            src="/public/images/photorestautest.png"
-            alt="photo-restau"
-          />
-        </section>
+      </section>
 
+      <section className="image-resto-plat">
+        <img
+          className="img-plat"
+          src="/public/images/plat.png"
+          alt="photo-plat"
+        />
+        <img
+          className="img-resto"
+          src="/public/images/resto.png"
+          alt="photo-restau"
+        />
+      </section>
+
+      <div className="reservation">
         <button type="button">réservation</button>
+      </div>
+      <div className="info-resto">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla tempora
+          esse doloribus eaque veniam, pariatur quam architecto at quaerat ipsa,
+          inventore dolor deleniti nam! Beatae doloribus dicta in veniam iste?
+        </p>
+      </div>
 
-        <div>
-          <button type="button">deja fait</button>
-          <button type="button">favoris</button>
-        </div>
+      <div className="fav-enr">
+        <button className="button-enr" type="button">
+          deja fait
+        </button>
+        <button className="button-fav" type="button">
+          favoris
+        </button>
+      </div>
 
-        <section>
-          <h1>Avis google</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
-            asperiores nihil illum deleniti similique nam, neque eum sit vel
-            facilis quod laboriosam obcaecati mollitia sint explicabo sunt,
-            incidunt quas laudantium.
-          </p>
-        </section>
+      <section className="avis">
+        <h1>Avis google</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
+          asperiores nihil illum deleniti similique nam, neque eum sit vel
+          facilis quod laboriosam obcaecati mollitia sint explicabo sunt,
+          incidunt quas laudantium.
+        </p>
+      </section>
 
-        <section>
-          <h1>Google map</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
-            quia a repellendus exercitationem labore, modi harum sunt soluta
-            quisquam assumenda neque atque cumque quas consequuntur repellat
-            ratione. Quaerat, corrupti tenetur.
-          </p>
-        </section>
-      </main>
+      <section className="google-map">
+        <h1>Google map</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+          quia a repellendus exercitationem labore, modi harum sunt soluta
+          quisquam assumenda neque atque cumque quas consequuntur repellat
+          ratione. Quaerat, corrupti tenetur.
+        </p>
+      </section>
+
       <footer>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate
         deleniti ab delectus quisquam accusamus quia alias est optio obcaecati,
