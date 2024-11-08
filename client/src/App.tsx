06ea26397a4d003d0b4e493ceeef9dc80c88aaf1
Emeric Lesage <elesage.dev@gmail.com>
2024-11-08 10:08:43 +0100

    created footer. Rest to link about us

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,18 @@ function App() {
   return (
     <>
       <Navbar />
+      <main />
+      <footer>
+        <p>&copy;</p>
+        <p>
+          About us
+          {/* il faudra mettre le composant en lien avec react router */}
+        </p>
+        <p>
+          emoji ?
+          {/* je  propose de rajouter un petit emoji de fourchette ou de couvert*/}
+        </p>
+      </footer>
     </>
   );
 }
