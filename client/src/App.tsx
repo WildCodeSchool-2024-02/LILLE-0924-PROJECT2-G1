e5242946463193e3212f4aded2d0595795ec613d
Julien Ido <idojulien202@gmail.com>
2024-11-12 12:02:12 +0100

    J'ai confectionné le formulaires de reservation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import CardRestaurant from "./components/card-restaurant/CardRestaurant";
 import Navbar from "./components/Navbar/Navbar";
+import Réservation from "./components/Réservation/Réservation";
+import CardRestaurant from "./components/card-restaurant/CardRestaurant";
 
 function App() {
   return (
     <>
       <Navbar />
       <CardRestaurant />
+      <Réservation />
     </>
   );
 }
