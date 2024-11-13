f0495f46c195ee73ce4aa26a57d5a1ed9d6c02c1
Foubert Antoine <ant.foub@gmail.com>
2024-11-13 11:20:44 +0100

    Update Carrousel.tsx
    Commit deleted

--- client/src/components/carrousel/Carrousel.tsx
+++ client/src/components/carrousel/Carrousel.tsx
@@ -7,13 +7,10 @@ interface imageFromApiProps {
 }
 
 function RandomImages() {
-  // j'ai crée un state qui va accueiller les images
   const [images, setImages] = useState();
 
-  // utilisation du hook useEffect pour faire une requête à l'API pixabay
-  // pour ne pas que ca fasse une boucle infinie. On met un tableau vide en deuxième paramètre  de useEffect
-  useEffect(() => {
-    // fetch pour récupérer les images de l'API pixabay
+   useEffect(() => {
+   
     fetch(
       "https://pixabay.com/api/?key=46964402-9754107f6087e15486eb4050e&q=food&image_type=photo",
     )
@@ -28,11 +25,7 @@ function RandomImages() {
         return setImages(newArrayImages);
       });
   }, []);
-
-  // j'ai crée un nouveau tableau pour stocker uniquement les liens d'images à affiches et pas les objets entiers
-
-  // j'ai mapé sur imagesFromApi.hits parce que c'est comme ça que l'objet reçu était structuré par l'api: { tatatite, hits:[tableau d'ojects qui m'intéresse...]}
-
+  
   return (
     <>
       <div className="carousel">
@@ -41,7 +34,7 @@ function RandomImages() {
           {
             images?.map((image: imageFromApiProps, index) => (
               <img src={image} key={`${index}-img`} alt="" />
-            )) /* afficher les images avec un map , et trouver une librairie qui fait ce que tu veux faire */
+            ))
           }
         </div>
         <div className="carousel-gradient gradient-right"></div>
