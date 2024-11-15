74e799baea404dd9e9fc174e06a0a01b6adbb2da
Lesage Emeric <elesage.dev@gmail.com>
2024-11-15 09:19:15 +0100

    Update App.tsx
    deleted useless import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import Carrousel from "./components/Carrousel/Carrousel";
-import Article from "./components/Article/Article";
 import Navbar from "./components/Navbar/Navbar";
 import CardRestaurants from "./components/card-restaurants/CardRestaurants";
 
