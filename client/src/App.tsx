efedc6237f0a50dbbdf7d3485c72dd5ba3740543
Julien Ido <idojulien202@gmail.com>
2024-11-15 10:03:48 +0100

    last modif

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import Réservation from "./components/Reservation/Reservation";
+import Reservation from "./components/Reservation/Reservation";
 import CardRestaurant from "./components/card-restaurant/CardRestaurant";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <CardRestaurant />
-      <Réservation />
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<CardRestaurant />
+			<Reservation />
+		</>
+	);
 }
 
 export default App;
