2df0acb26b822cd14975a0fbb10e1e11f339b24a
Emeric Lesage <elesage.dev@gmail.com>
2024-11-18 10:33:31 +0100

    started to fix indentation and classnames

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,9 @@
 import "./App.css";
+import { Link } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import AboutUs from "./components/about-us/AboutUs";
 import Article from "./components/article/Article";
 import CardRestaurants from "./components/card-restaurants/CardRestaurants";
 import Carrousel from "./components/carrousel/Carrousel";
-import { Link } from "react-router-dom";
 
 function App() {
   return (
