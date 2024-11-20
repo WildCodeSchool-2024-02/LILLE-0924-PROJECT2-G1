118e28f64b1b85799124ab707a93cb6f76f2e8c1
ANTOINE <ANT.FOUB@GMAIL.COM>
2024-11-20 10:29:39 +0100

    commit before merge

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,16 @@
 import "./App.css";
 import { Link } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-<<<<<<< HEAD
-import CuisineFilterButtons from "./components/buttonCategory/CuisineFilterButtons";
-import CardRestaurant from "./components/card-restaurant/CardRestaurant";
-=======
 import Article from "./components/article/Article";
+import CuisineFilterButtons from "./components/buttonCategory/CuisineFilterButtons";
 import CardRestaurants from "./components/card-restaurants/CardRestaurants";
 import Carrousel from "./components/carrousel/Carrousel";
->>>>>>> dev
 
 function App() {
   return (
     <>
       <Navbar />
-<<<<<<< HEAD
       <CuisineFilterButtons restaurantsList={[]} />
-      <CardRestaurant />
-=======
       <Carrousel />
       <CardRestaurants />
       <Article />
@@ -25,7 +18,6 @@ function App() {
         <p>&copy;</p>
         <Link to="/about">About</Link>
       </footer>
->>>>>>> dev
     </>
   );
 }
