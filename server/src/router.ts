b4f636bd5e72c88c4d35c00270a80972fb888837
Julien Ido <idojulien202@gmail.com>
2024-11-20 14:48:37 +0100

    Modification du router ainsi que du composant Article.tsx

--- server/src/router.ts
+++ server/src/router.ts
@@ -8,14 +8,14 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Define item-related routes
-router.get("/restau", (req, res) => {
+router.get("/restaurant", (req, res) => {
   res.send(data);
 });
 
-router.get("/restau/:id", (req, res) => {
+router.get("/restaurant/:id", (req, res) => {
   const id = req.params.id;
   const dataFiltered = data.restaurants.filter(
-    (restau) => restau.id === Number.parseInt(id),
+    (restaurant) => restaurant.id === Number.parseInt(id),
   );
   res.send(dataFiltered[0]);
 });
