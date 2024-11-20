ac98a037aace6fa6423f7246750c465ef94a1f63
Julien Ido <idojulien202@gmail.com>
2024-11-20 16:54:55 +0100

    Création d'un footer propre et simple

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,6 @@
 import "./App.css";
-import { Link } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Article from "./components/article/Article";
-import CardRestaurants from "./components/card-restaurants/CardRestaurants";
-import Carrousel from "./components/carrousel/Carrousel";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Carrousel />
-      <CardRestaurants />
-      <Article />
-      <footer>
-        <p>&copy;</p>
-        <Link to="/about">About</Link>
-      </footer>
-    </>
-  );
+  return <></>;
 }
 export default App;
