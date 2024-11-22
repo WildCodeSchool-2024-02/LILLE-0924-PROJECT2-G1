dd5290a0442d6fe7e4568b98634785668a58f1c7
Emeric Lesage <elesage.dev@gmail.com>
2024-11-22 15:15:15 +0100

    changed import to import type

--- client/src/components/restaurants-list/restaurantList.tsx
+++ client/src/components/restaurants-list/restaurantList.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
+import type { restaurantProps } from "../../types/RestaurantType";
 import CardRestaurants from "../cardRestaurants/CardRestaurants";
 import Searchbar from "../searchbar/SearchBar";
-import { restaurantProps } from "../../types/RestaurantType";
 
 function RestaurantsList() {
   const [restaurantsList, setRestaurantsList] = useState<restaurantProps[]>([]);
@@ -26,7 +26,7 @@ function RestaurantsList() {
             restaurant.name.toLowerCase().includes(search.toLowerCase()) ||
             restaurant.cuisine.toLowerCase().includes(search.toLowerCase())
           );
-        })
+        }),
       );
     }
   }, [search, restaurantsList]);
@@ -35,7 +35,7 @@ function RestaurantsList() {
       <Searchbar search={search} setSearch={setSearch} />
       {/* la meme chose ici avec le filter */}
       <CardRestaurants
-        restaurantsList={search ? restaurantsBySearch ?? [] : restaurantsList}
+        restaurantsList={search ? (restaurantsBySearch ?? []) : restaurantsList}
       />
     </>
   );
